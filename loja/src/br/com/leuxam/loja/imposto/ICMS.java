24d1db4d4b9ddf54be017659ec715c6b31fbdc94
Maxuel Junior <maxueltstz@hotmail.com>
2023-07-06 13:32:07 -0300

    Implementando padrão de projeto Decorator

--- loja/src/br/com/leuxam/loja/imposto/ICMS.java
+++ loja/src/br/com/leuxam/loja/imposto/ICMS.java
@@ -4,8 +4,13 @@ import java.math.BigDecimal;
 
 import br.com.leuxam.loja.orcamento.Orcamento;
 
-public class ICMS implements Imposto{
-	public BigDecimal calcular(Orcamento orcamento) {
+public class ICMS extends Imposto{
+	
+	public ICMS(Imposto outro) {
+		super(outro);
+	}
+
+	public BigDecimal realizarCalculo(Orcamento orcamento) {
 		return orcamento.getValor().multiply(new BigDecimal("0.1"));
 	}
 }
--- loja/src/br/com/leuxam/loja/imposto/Imposto.java
+++ loja/src/br/com/leuxam/loja/imposto/Imposto.java
@@ -4,6 +4,22 @@ import java.math.BigDecimal;
 
 import br.com.leuxam.loja.orcamento.Orcamento;
 
-public interface Imposto {
-	BigDecimal calcular(Orcamento orcamento);
+public abstract class Imposto {
+	
+	private Imposto outro;
+	
+	public Imposto(Imposto outro) {
+		this.outro = outro;
+	}
+
+	protected abstract BigDecimal realizarCalculo(Orcamento orcamento);
+	
+	public BigDecimal calcular(Orcamento orcamento) {
+		BigDecimal valorImposto = realizarCalculo(orcamento);
+		BigDecimal valorDoOutroImposto = BigDecimal.ZERO;
+		if(outro != null) {
+			valorDoOutroImposto = outro.realizarCalculo(orcamento);
+		}
+		return valorImposto.add(valorDoOutroImposto);
+	}
 }
